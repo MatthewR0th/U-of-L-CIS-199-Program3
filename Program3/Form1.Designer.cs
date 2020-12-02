fcb3780a9ffbf5b9792a10a2738f19cf3ae90625
Matthew J Roth <mjroth03@louisville.edu>
2020-12-02 13:20:16 -0500

    Add project files.

---
+++ Program3/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Program3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stateNameLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.productNameLabel = new System.Windows.Forms.Label();
+            this.initialCostLabel = new System.Windows.Forms.Label();
+            this.discountedCostLabel = new System.Windows.Forms.Label();
+            this.initialCostOutput = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.discountedCostOutput = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalPriceOutput = new System.Windows.Forms.Label();
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.productTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.calculationButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // stateNameLabel
+            // 
+            this.stateNameLabel.AutoSize = true;
+            this.stateNameLabel.Location = new System.Drawing.Point(72, 46);
+            this.stateNameLabel.Name = "stateNameLabel";
+            this.stateNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.stateNameLabel.TabIndex = 0;
+            this.stateNameLabel.Text = "State:";
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(72, 132);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(46, 13);
+            this.quantityLabel.TabIndex = 1;
+            this.quantityLabel.Text = "Quantity";
+            // 
+            // productNameLabel
+            // 
+            this.productNameLabel.AutoSize = true;
+            this.productNameLabel.Location = new System.Drawing.Point(71, 78);
+            this.productNameLabel.Name = "productNameLabel";
+            this.productNameLabel.Size = new System.Drawing.Size(47, 13);
+            this.productNameLabel.TabIndex = 2;
+            this.productNameLabel.Text = "Product:";
+            // 
+            // initialCostLabel
+            // 
+            this.initialCostLabel.AutoSize = true;
+            this.initialCostLabel.Location = new System.Drawing.Point(72, 227);
+            this.initialCostLabel.Name = "initialCostLabel";
+            this.initialCostLabel.Size = new System.Drawing.Size(58, 13);
+            this.initialCostLabel.TabIndex = 3;
+            this.initialCostLabel.Text = "Initial Cost:";
+            // 
+            // discountedCostLabel
+            // 
+            this.discountedCostLabel.AutoSize = true;
+            this.discountedCostLabel.Location = new System.Drawing.Point(72, 267);
+            this.discountedCostLabel.Name = "discountedCostLabel";
+            this.discountedCostLabel.Size = new System.Drawing.Size(88, 13);
+            this.discountedCostLabel.TabIndex = 4;
+            this.discountedCostLabel.Text = "Discounted Cost:";
+            // 
+            // initialCostOutput
+            // 
+            this.initialCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.initialCostOutput.Location = new System.Drawing.Point(223, 219);
+            this.initialCostOutput.Name = "initialCostOutput";
+            this.initialCostOutput.Size = new System.Drawing.Size(104, 21);
+            this.initialCostOutput.TabIndex = 5;
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Location = new System.Drawing.Point(72, 350);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(61, 13);
+            this.totalPriceLabel.TabIndex = 6;
+            this.totalPriceLabel.Text = "Total Price:";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(72, 308);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(28, 13);
+            this.taxLabel.TabIndex = 7;
+            this.taxLabel.Text = "Tax:";
+            // 
+            // discountedCostOutput
+            // 
+            this.discountedCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.discountedCostOutput.Location = new System.Drawing.Point(223, 267);
+            this.discountedCostOutput.Name = "discountedCostOutput";
+            this.discountedCostOutput.Size = new System.Drawing.Size(104, 21);
+            this.discountedCostOutput.TabIndex = 12;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.taxOutput.Location = new System.Drawing.Point(223, 319);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(104, 21);
+            this.taxOutput.TabIndex = 13;
+            // 
+            // totalPriceOutput
+            // 
+            this.totalPriceOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalPriceOutput.Location = new System.Drawing.Point(223, 350);
+            this.totalPriceOutput.Name = "totalPriceOutput";
+            this.totalPriceOutput.Size = new System.Drawing.Size(104, 21);
+            this.totalPriceOutput.TabIndex = 14;
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Items.AddRange(new object[] {
+            "KY",
+            "OH",
+            "IN",
+            "IL"});
+            this.stateComboBox.Location = new System.Drawing.Point(191, 38);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(121, 21);
+            this.stateComboBox.TabIndex = 15;
+            // 
+            // productTextBox
+            // 
+            this.productTextBox.Location = new System.Drawing.Point(191, 78);
+            this.productTextBox.Name = "productTextBox";
+            this.productTextBox.Size = new System.Drawing.Size(121, 20);
+            this.productTextBox.TabIndex = 16;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(191, 125);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(121, 20);
+            this.quantityTextBox.TabIndex = 17;
+            // 
+            // calculationButton
+            // 
+            this.calculationButton.Location = new System.Drawing.Point(223, 171);
+            this.calculationButton.Name = "calculationButton";
+            this.calculationButton.Size = new System.Drawing.Size(75, 23);
+            this.calculationButton.TabIndex = 18;
+            this.calculationButton.Text = "Calculate";
+            this.calculationButton.UseVisualStyleBackColor = true;
+            this.calculationButton.Click += new System.EventHandler(this.calculationButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 413);
+            this.Controls.Add(this.calculationButton);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.productTextBox);
+            this.Controls.Add(this.stateComboBox);
+            this.Controls.Add(this.totalPriceOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.discountedCostOutput);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.initialCostOutput);
+            this.Controls.Add(this.discountedCostLabel);
+            this.Controls.Add(this.initialCostLabel);
+            this.Controls.Add(this.productNameLabel);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.stateNameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label stateNameLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label productNameLabel;
+        private System.Windows.Forms.Label initialCostLabel;
+        private System.Windows.Forms.Label discountedCostLabel;
+        private System.Windows.Forms.Label initialCostOutput;
+        private System.Windows.Forms.Label totalPriceLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label discountedCostOutput;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalPriceOutput;
+        private System.Windows.Forms.ComboBox stateComboBox;
+        private System.Windows.Forms.TextBox productTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.Button calculationButton;
+    }
+}
+
---
+++ Program3/Form1.cs
@@ -0,0 +1,194 @@
+﻿//Grading ID: K1791
+//Program 3
+//5 November 2020
+//CIS 199-02
+//This assignment explores parallel arrays, range matching.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //When the calculate button is pressed, the following calculations will take place
+        private void calculationButton_Click(object sender, EventArgs e)
+        {
+            
+
+            string[] productNumbers = { "1001", "1002", "1003", "1004", "1005", "1006", "1007", }; //Values of product numbers
+            double[] costPerUnit = { 7.78, 9.51, 10.73 , 9.99 , 11.99, 5.00, 4.58 }; //Values of cost per units
+
+
+
+            string[] states = { "KY", "OH", "IN", "IL", }; //List of four states
+            stateComboBox.Items.AddRange(states); //Adds the array of states to the combobox
+            double[] tax = { 0.06, 0.0717, 0.07, 0.0874, }; //A list of sales taxes, already converted to useful forms
+
+            string[] items = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20" }; //How many items the user has input
+            double[] discount = { 0, 0, 0, 0, 0, 0.05, 0.05, 0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.15 }; //The discount that is given based on the number of items sold
+
+
+
+            string userProductNumber = productTextBox.Text; //What product number did the user use?
+            string userQuantityNumber = quantityTextBox.Text; //How many items did the user get?
+           
+            int firstCost; //An int to calculate the initial cost
+
+            firstCost = Convert.ToInt32(userQuantityNumber); //Converted the user input into a form that can be used for the calculations
+
+            string userState = stateComboBox.Text; //What state did the user select?
+
+
+
+            int index1 = 0; //A simple way to create the most basic of while loops
+            int index2 = 0; //A simple way to create the most basic of while loops
+            int index3 = 0; //A simple way to create the most basic of while loops
+
+            double cutOffValue1; //A simple way to create the most basic of while loops
+            double cutOffValue2; //A simple way to create the most basic of while loops
+            double cutOffValue3; //A simple way to create the most basic of while loops
+
+
+
+            bool productFound = false; //A simple way to create the most basic of while loops
+            bool quantityFound = false; //A simple way to create the most basic of while loops
+            bool stateFound = false; //A simple way to create the most basic of while loops
+
+            double finalCostPerUnit = 0; //Assigning a value of zero, that will get reassigned eventually if the while loop executes correctly.
+            double finalCostPerQuantity = 0; //Assigning a value of zero, that will get reassigned eventually if the while loop executes correctly.
+            double finalCostPerState = 0; //Assigning a value of zero, that will get reassigned eventually if the while loop executes correctly.
+
+            //Determines if the value of the product number textbox is valid, and if so assigns it to a variable
+
+            
+
+
+            while (index1 < productNumbers.Length && !productFound) //While index1 is less than the length of product numbers, and product number is false..
+            {
+                if (productNumbers[index1] == userProductNumber)
+
+                {
+                    productFound = true; //Updates the product found to true
+                    cutOffValue1 = costPerUnit[index1]; //The cutoff value has now been assigned the value of the matching cost per unit based on the product array
+                    finalCostPerUnit = cutOffValue1; //We now have a usable value, which is assigned from the above cutoffvalue1
+                    
+
+                }
+                else
+                {
+                    
+                    ++index1;
+                }
+            }
+
+           //Determines if the value of the items textbox is valid, and if so assigns it to a variable
+           
+            while (index2 < items.Length && !quantityFound) //While index2 is less than the length of the item array, and quantity number is false..
+            {
+                if (items[index2] == userQuantityNumber) //If the user input a quanity that matches the items array...
+
+                {
+
+                    quantityFound = true; //Updates the quantity found to true
+                    cutOffValue2 = discount[index2]; //The cutoff value has now been assigned the value of the discount that matches up with the quantity array
+                    finalCostPerQuantity= cutOffValue2; //We now have a usable value, which is assigned from the above cutoffvalue2
+
+                }
+               
+                else
+                {
+                    ++index2; //Updates index 2 count
+                }
+            }
+
+            //Determines if the value of the state combobox is valid, and if so assigns it to a variable
+
+
+            while (index3 < states.Length && !stateFound) //While index3 is less than the length of the states array, and state number is false...
+            {
+                if (states[index3] == userState) //If the value that the user input is in the states array...
+
+                {
+                    stateFound = true; //Statesfound has been updated to true
+                    cutOffValue3 = tax[index3]; //The cutoff value has now been assigned the matching tax value as compared to the states array
+                    finalCostPerState = cutOffValue3; //We now have a usable value, which is assigned from the above cutoffvalue3
+
+                }
+                else //If the above is not true..
+                {
+                    ++index3; //Updates the index3 count
+                }
+            }
+
+
+
+            double initialCalculation; //The initial calculation before any taxes or discounts are applied
+            double discountedCalculation; //The discounted calculation 
+            double trueAndHonestDiscount; //The actual discounted value
+            double taxCalculation; //The calculation that involves taxes
+            double totalCostCalculation;//The final calculation
+
+            if (firstCost > 20) // If the user has input more than 20 items...
+            {
+                finalCostPerQuantity = 0.15; //Assigns this value if items are over 20
+            }
+
+
+
+
+            initialCalculation = firstCost *  finalCostPerUnit; //Multiples the value found by the first loop, by how the quanity the user input in the quanity textbox
+            initialCostOutput.Text = initialCalculation.ToString("C2"); //Outputs the above value to the initial cost output, with 2 decimals of precision.
+
+            discountedCalculation = initialCalculation * finalCostPerQuantity; //Discount calculaton is equal to the initial cost multiplied by the discount based on how many items that were purchased
+
+            trueAndHonestDiscount = initialCalculation - discountedCalculation; //Gives an actual discounted value by subtracting the calculation above from the initial calculation
+
+            discountedCostOutput.Text = trueAndHonestDiscount.ToString("C2"); //Outputs the above to the label
+
+
+            taxCalculation = trueAndHonestDiscount * finalCostPerState; //Multiplies the discounted price by the tax value, which was found from a while loop
+
+            taxOutput.Text = taxCalculation.ToString("C2"); //Outputs the above calculation to a label
+
+            totalCostCalculation = taxCalculation + trueAndHonestDiscount; //The tax calculation, plus the discount is equal to the total
+
+            totalPriceOutput.Text = totalCostCalculation.ToString("C2"); //Displays the above calculation to a label
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
+
